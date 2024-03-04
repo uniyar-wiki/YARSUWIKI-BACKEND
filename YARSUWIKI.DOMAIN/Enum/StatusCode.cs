bc7cbfb12f60e6e1cc70a5ba4bcc04fa1eb3a2a5
Егор Полетаев <defaultgg@inbox.ru>
2024-03-04 20:50:19 +0300

    storage controller layout

--- YARSUWIKI.DOMAIN/Enum/StatusCode.cs
+++ YARSUWIKI.DOMAIN/Enum/StatusCode.cs
@@ -2,6 +2,8 @@ namespace YARSUWIKI.DOMAIN.Enum;
 
 public enum StatusCode
 {
+    FileAlreadyExists = 409,
+    FileNotFound = 404,
     UserNotFound = 0,
     UserAlreadyExists=409,
     OK = 200,
---
+++ YARSUWIKI.SERVICE/Interfaces/IStorageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YARSUWIKI.DOMAIN.Response;
+
+namespace YARSUWIKI.SERVICE.Interfaces
+{
+    public interface IStorageService
+    {
+        Task<BaseResponse<bool>> Download(string fileName);
+
+        Task<BaseResponse<bool>> Upload(FileInfo file);
+    }
+}
