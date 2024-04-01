08487af83732334b79e58819ea53f5c7ef6d4698
overloaded <money.flow@inbox.ru>
2024-04-01 19:53:30 +0300

    Migrations

---
+++ MigrationsProject/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.EntityFrameworkCore.Design;
+using YARSUWIKI.DOMAIN.Entity;
+
+namespace Migrations
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=yarsuwiki;Username=nikita;Password=dolbayeb");
+        }
+    }
+}
