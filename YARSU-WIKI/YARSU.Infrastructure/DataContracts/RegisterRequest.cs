f7542b9cd9406e0e3270f99243f272ba04f00b99
overloaded <93100630+dddosha341@users.noreply.github.com>
2024-05-24 20:05:11 +0300

    Add files via upload

---
+++ YARSU-WIKI/YARSU.Infrastructure/DataContracts/RegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace YARSU.Infrastructure.DataContracts;
+
+public class RegisterRequest
+{
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+}
+
---
+++ YARSU-WIKI/YARSU.Infrastructure/DataContracts/ValidateTokenResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace YARSU.Infrastructure.DataContracts;
+
+public class ValidateTokenResponse
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; }
+}
+
